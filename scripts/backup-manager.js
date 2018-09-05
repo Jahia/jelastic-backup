4388072d878dbc660f53e41225e9cab37b19b6f3
nishgupta29 <39030732+nishgupta29@users.noreply.github.com>
2018-09-05 12:51:20 -0400

    Update backup-manager.js

--- scripts/backup-manager.js
+++ scripts/backup-manager.js
@@ -86,8 +86,8 @@ function BackupManager(config) {
             [ me.checkEnvStatus ],
             [ me.cmd, [
                 lftp.cmd([
-                    "mkdir %(envName)",
-                    "mkdir %(envName)/%(backupDir)"
+                    "sudo mkdir %(envName)",
+                    "sudo mkdir %(envName)/%(backupDir)"
                 ]),
                 'wget --http-user=${MANAGER_USER} --http-password=${MANAGER_PASSWORD} -O - %(maintenanceUrl)=true',
                 'tar -zcf data.tar.gz /data',
